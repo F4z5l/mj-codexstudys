aadfb8777fbf94b93f6035b4823df2243b7f897f
Fazal <ffyf6525@gmail.com>
2026-09-03 20:06:18 +0530

    Delete server directory

--- server/controllers/upload.controller.js
+++
@@ -1,5 +0,0 @@
-// server/controllers/upload.controller.js
-exports.uploadPDF = (req, res) => {
-    if (!req.file) return res.status(400).json({ success: false, message: "No file uploaded" });
-    res.json({ success: true, filename: req.file.filename });
-};
